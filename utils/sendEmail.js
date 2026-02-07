6f2b8f316389fbd63a0dc21399dd694000373a02
khizer08 <syedkhizer2004@gmail.com>
2026-02-07 20:16:57 +0530

    Email-feature-nodemailer

---
+++ utils/sendEmail.js
@@ -0,0 +1,24 @@
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+const sendEmail = async ({ to, subject, html }) => {
+  try {
+    await transporter.sendMail({
+      from: `"Wanderlust" <${process.env.EMAIL_USER}>`,
+      to,
+      subject,
+      html,
+    });
+  } catch (err) {
+    console.error("Email error:", err.message);
+  }
+};
+
+module.exports = sendEmail;
