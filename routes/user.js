b4fdfcb6a7ce6585517aa01a54a7e6ed742b9f7e
khizer08 <syedkhizer2004@gmail.com>
2026-02-09 22:10:08 +0530

    email-verification-routes

--- routes/user.js
+++ routes/user.js
@@ -1,32 +1,36 @@
-const express=require("express");
-const router=express.Router();
-const User=require("../models/user.js");
+const express = require("express");
+const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync");
-const passport=require("passport");
+const passport = require("passport");
 const { saveRedirectUrl } = require("../middleware.js");
-const userController=require("../controllers/users.js");
+const userController = require("../controllers/users.js");
 
-
-// SignUp GET and POST route 
+// SignUp GET and POST route
 router
-.route("/signup")
-.get(userController.renderSignupForm)
-.post(wrapAsync(userController.signup)
-);
+  .route("/signup")
+  .get(userController.renderSignupForm)
+  .post(wrapAsync(userController.signup));
 
-
-// Login GET and POST route 
+// Login GET and POST route
 router
-.route("/login")
-.get(userController.renderLoginForm)
-.post(saveRedirectUrl,
-    passport.authenticate("local",{failureRedirect:"/login",failureFlash:true}),
-    wrapAsync(userController.login)
-);
-
+  .route("/login")
+  .get(userController.renderLoginForm)
+  .post(
+    saveRedirectUrl,
+    passport.authenticate("local", {
+      failureRedirect: "/login",
+      failureFlash: true,
+    }),
+    wrapAsync(userController.login),
+  );
 
 // Logout
-router.get("/logout",userController.logout);
+router.get("/logout", userController.logout);
 
+// EMAIL OTP VERIFICATION ROUTES
+router
+  .route("/verify-email")
+  .get(userController.renderVerifyEmailForm)
+  .post(wrapAsync(userController.verifyEmail));
 
-module.exports=router;
+module.exports = router;
