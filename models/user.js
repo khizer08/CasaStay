913e52be8ca47b0340686a4147af719ac999abd5
khizer08 <syedkhizer2004@gmail.com>
2026-02-09 16:59:16 +0530

    user-schema-update

--- models/user.js
+++ models/user.js
@@ -1,15 +1,34 @@
-const mongoose=require("mongoose");
-const Schema=mongoose.Schema;
-const passportLocalMongoose=require("passport-local-mongoose");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require("passport-local-mongoose");
 
-const userSchema=new Schema({
-    email:{
-        type:String,
-        required:true
-    }
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+
+  //Email Verification Fields.
+  isEmailVerified: {
+    type: Boolean,
+    default: false,
+  },
+
+  emailOTPHash: {
+    type: String,
+  },
+
+  emailOTPExpires: {
+    type: Date,
+  },
+
+  emailOTPAttempts: {
+    type: Number,
+    default: 0,
+  },
 });
+// till here
 
-userSchema.plugin(passportLocalMongoose);// "plugin" automatically creates "username,salting,hashing" for us.
+userSchema.plugin(passportLocalMongoose);
 
-module.exports = mongoose.model('User', userSchema);
-
+module.exports = mongoose.model("User", userSchema);
