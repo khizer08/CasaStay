ad25e3c3441d55a0924130ab5d9f28d9cb5ab608
khizer08 <syedkhizer2004@gmail.com>
2026-02-09 21:50:39 +0530

    otp-email-template

---
+++ views/emails/otpStyle.js
@@ -0,0 +1,23 @@
+module.exports = {
+  container: `
+    max-width: 500px;
+    margin: auto;
+    padding: 20px;
+    font-family: Arial, sans-serif;
+  `,
+  heading: `
+    color: #ff385c;
+    text-align: center;
+  `,
+  otpText: `
+    text-align: center;
+    font-size: 28px;
+    letter-spacing: 6px;
+    margin: 20px 0;
+  `,
+  footer: `
+    font-size: 12px;
+    color: #777;
+    text-align: center;
+  `,
+};
