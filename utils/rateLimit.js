bbb4d34ba038e8eff8a8345b9fc988c28dc89031
khizer08 <syedkhizer2004@gmail.com>
2026-02-15 11:39:02 +0530

    Added Mongo rate limit store

--- utils/rateLimit.js
+++ utils/rateLimit.js
@@ -1,16 +1,23 @@
 const rateLimit = require("express-rate-limit");
+const MongoStore = require("rate-limit-mongo");
+require("dotenv").config();
 
 const createOtpResendLimiter = (redirectPathFn) => {
   return rateLimit({
-    windowMs: 10 * 60 * 1000, // 10 Minutes
+    windowMs: 10 * 60 * 1000,
     max: 3,
+
+    store: new MongoStore({
+      uri: process.env.ATLASDB_URL,
+      collectionName: "rateLimitResendOTP",
+    }),
+
     standardHeaders: true,
     legacyHeaders: false,
 
     handler: (req, res) => {
       const resetTime = req.rateLimit?.resetTime;
-
-      let minutesLeft = 10; // fallback
+      let minutesLeft = 10;
 
       if (resetTime) {
         const remainingMs = resetTime - Date.now();
