28379f36862e1cec3920d997c5b2f961ac5f34a6
khizer08 <syedkhizer2004@gmail.com>
2026-02-16 23:32:10 +0530

    Schema-bug-fix

--- schema.js
+++ schema.js
@@ -9,6 +9,22 @@ module.exports.listingSchema = Joi.object({
     country: Joi.string().required(),
     price: Joi.number().required().min(0),
     image: Joi.string().allow("", null),
+    category: Joi.string()
+      .valid(
+        "Trending",
+        "Rooms",
+        "Iconic Cities",
+        "Mountains",
+        "Castles",
+        "Amazing Pools",
+        "Camping",
+        "Farms",
+        "Arctic",
+        "Domes",
+        "Boats",
+      )
+      .allow("", null)
+      .optional(),
   }).required(),
 });
 
